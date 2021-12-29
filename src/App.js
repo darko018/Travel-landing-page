2e1b3fcfc5736aae89a8ced07507277ee6d07a38
Darko Kovačević <darkokovacevic018@gmail.com>
2021-12-29 15:10:33 +0100

    'app finished'

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import travel_01 from "./assets/travel-01.jpg";
+import travel_02 from "./assets/travel-02.jpg";
+import travel_03 from "./assets/travel-03.jpg";
+import travel_04 from "./assets/travel-04.jpg";
+import travel_05 from "./assets/travel-05.jpg";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+
+function App() {
+  const navbarLinks = [
+    { url: "#", title: "Home" },
+    { url: "#", title: "Trips" },
+    { url: "#", title: "Rewards" },
+  ];
+
+  return (
+    <div className="App">
+      <Navbar navbarLinks={navbarLinks} />
+      <Hero imageSrc={travel_01} />
+      <Slider
+        imageSrc={travel_02}
+        title={"Be an explorer."}
+        subtitle={
+          "Our platform offers a wide variety of unique travel locations!"
+        }
+      />
+      <Slider
+        imageSrc={travel_03}
+        title={"Enjoy your life."}
+        subtitle={"Your dream vacation is only a few clicks away."}
+        flipped={true}
+      />
+      <Slider
+        imageSrc={travel_04}
+        title={"Free yourself."}
+        subtitle={"Your dream vacation is only a few clicks away."}
+        // flipped={true}
+      />
+      <Slider
+        imageSrc={travel_05}
+        title={"Memories for a lifetime."}
+        subtitle={"Your dream vacation is only a few clicks away."}
+        flipped={true}
+      />
+    </div>
+  );
+}
+
+export default App;
